79c0e89c2be545e518ce0159eee8132ae09a7b8d
xfuryred <57618758+xfuryred@users.noreply.github.com>
2025-06-29 01:02:48 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-  return Intl.DateTimeFormat("en-US", {
+  return Intl.DateTimeFormat("it-IT", {
     year: "numeric",
     month: "long",
     day: "numeric",
